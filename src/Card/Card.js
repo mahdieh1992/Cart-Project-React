7e3d7474741144e6225f602f45fbe89b0f37caab
mahdieh1992 <mohamadimahdieh70@gmail.com>
2024-05-21 18:02:58 +0330

    props destructuring

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,17 +1,18 @@
 import './Card.css'
 
-export default function Card(props) {
+export default function Card({Title,Image,Description,Price}) {
+
+    // let {Title,Image,Description,Price}=props
     return (
-        props.Price > 500 &&
+       Price > 500 &&
         <div className='contain'>
 
-            <h1 id='hProduct'>{props.Title}</h1>
+            <h1 id='hProduct'>{Title}</h1>
             <div className="Card">
-                <img src={props.Image}></img>
-                <p>{props.Description}</p>
-                <p id='price'>{props.Price}</p>
+                <img src={Image}></img>
+                <p>{Description}</p>
+                <p id='price'>{Price}</p>
                 <button id="btnCard">CardPay</button>
-
             </div>
 
         </div>
